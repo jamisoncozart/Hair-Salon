f9016e4c86d0607f6f6b127242a9e6bbfaa1998a
Jamison Cozart <jamisoncozart@gmail.com>
2020-03-20 10:16:52 -0700

    Update Models to contain all necessary properties to reflect DB

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -2,6 +2,9 @@ namespace HairSalon.Models
 {
   public class Client
   {
-    // code
+    public int ClientId { get; set; }
+    public string Name { get; set; }
+    public string Haircut { get; set; }
+    public int StylistId { get; set; }
   }
 }
